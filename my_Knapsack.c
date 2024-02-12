4775e704d80655041aa926fc3c00c3d82945d602
Y.P <pardes.yoyo@gmail.com>
2024-02-12 20:11:31 +0200

    Add comment and cleanup the code

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-#define BACKPACKSIZE 5
+#define BACKPACKSIZE 5 // The maximum number of items
 #define BACKPACKMAXWEIGHT 20
 
 
-
+/**
+ * fill the lookup table for the knapsack problem
+ * @param stored_values the lookup table
+ * @param weights the inputted weights accordingly
+ * @param values  the values of each item accordingly
+ */
 void fill_values(int stored_values[BACKPACKSIZE + 1][BACKPACKMAXWEIGHT + 1], int weights[], int values[])
 {
 
-	// Build table K[][] in bottom up manner
+	// Filling the lookup table
 	for (int i = 0; i <= BACKPACKSIZE; i++)
 	{
 		for (int w = 0; w <= BACKPACKMAXWEIGHT; w++)
 		{
-			if (i==0 || w==0)
+			if (i==0 || w==0) // the base case no items left or no weights left
 			{
-				stored_values[i][w] = 0;
+				stored_values[i][w] = 0; // zero is assigned
 			}
 
-			else if (weights[i-1] <= w)
+
+			else if (weights[i-1] <= w) // if there is enough space
 			{
+				// the maximum value is assigned
 				stored_values[i][w] = values[i-1] + stored_values[i-1][w-weights[i-1]] > stored_values[i-1][w] ?
 									  values[i-1] + stored_values[i-1][w-weights[i-1]] : stored_values[i-1][w];
 			}
-
-			else
+			else // if there is not enough place
 			{
-				stored_values[i][w] = stored_values[i - 1][w];
+				stored_values[i][w] = stored_values[i - 1][w]; // we continue with one less item
 			}
 		}
 	}
 }
 
+/**
+ * This function fill the items, weights, and values acoording to the user input
+ */
 void process_inputs(char items[], int weights[], int values[])
 {
     for (int i = 0; i < BACKPACKSIZE; ++i)
@@ -49,103 +58,87 @@ void process_inputs(char items[], int weights[], int values[])
  */
 void get_items(int i, int j, int weights[], int selected_bool[], int stored_values[BACKPACKSIZE + 1][BACKPACKMAXWEIGHT + 1])
 {
-	if (i == 0)
+	if (i == 0) // if we went through all the items
 	{
 		return;
 	}
 
-	if (stored_values[i][j] > stored_values[i-1][j])
+	if (stored_values[i][j] > stored_values[i-1][j]) // if the value WITH the items is greater than the value without
 	{
-		selected_bool[i] = 1;
-		get_items(i-1, j-weights[i], weights, selected_bool, stored_values);
+		selected_bool[i-1] = 1; // we have take the item
+		get_items(i-1, j - weights[i-1], weights, selected_bool, stored_values); // recursive call with the weight reduced by the taken items
 	}
-	else
+	else // if the value is same without the object
 	{
-		get_items(i-1, j, weights, selected_bool, stored_values);
+		get_items(i-1, j, weights, selected_bool, stored_values); // recursive call without the object
 	}
-
 }
 
-char * create_string(char string[11], int selected_bool[], char items[], int weights[]){
+/**
+ * given a boolean array build the string for the output
+ * @return the built string
+ */
+char * create_string(char string[11], int selected_bool[], char items[]){
 
 	int string_index = 0;
-	for (int i = 0; i < BACKPACKSIZE; ++i) {
-		if (selected_bool[i])
+	for (int i = 0; i < BACKPACKSIZE; ++i)
+	{
+		if (selected_bool[i]) // if the boolean is 1 at this index
 		{
-			string[string_index] = items[i];
-			string[string_index + 1] = ' ';
-			string_index += 2;
+			string[string_index] = items[i]; // add the item
+			string[string_index + 1] = ' '; // add a space
+			string_index += 2; // increment the index by two to not override
 		}
 	}
 	string[string_index] = '\0'; // ending the string
 	return string;
 }
 
+/**
+ * The main function which solve the knapsack problem using an iterative solution
+ *	@return the maximum value of the bag computed
+ */
 int knapSack(int weights[], int values[], int selected_bool[])
 {
 	int stored_values[BACKPACKSIZE + 1][BACKPACKMAXWEIGHT + 1];
 
-	for (int i = 0; i <= BACKPACKSIZE; ++i) {
-		for (int j = 0; j <= BACKPACKMAXWEIGHT; ++j) {
+	// starting the lookup table with -1
+	for (int i = 0; i <= BACKPACKSIZE; ++i)
+	{
+		for (int j = 0; j <= BACKPACKMAXWEIGHT; ++j)
+		{
 			stored_values[i][j] = -1;
 		}
 	}
 
 	fill_values(stored_values, weights, values); // fill the matrix with optimal solutions
-
 	get_items(BACKPACKSIZE, BACKPACKMAXWEIGHT, weights, selected_bool, stored_values); //update the boolean
-
-	printf("\n");
-	for (int i = 0; i <=BACKPACKSIZE; ++i) {
-		for (int j = 0; j <= BACKPACKMAXWEIGHT; ++j) {
-			printf("%i ", stored_values[i][j]);
-		}
-		printf("\n");
-	}
 
 	return stored_values[BACKPACKSIZE][BACKPACKMAXWEIGHT]; // the maximum value
 }
 
-
+/**
+ * The main function of the program, take the input of the user and precess the optimal solution for the knapsack problem
+ * then print the results
+ */
 int main()
 {
+	// array initialisation
 	char items[BACKPACKSIZE];
 	int weights[BACKPACKSIZE];
 	int values[BACKPACKSIZE];
 	int selected_bool[BACKPACKSIZE] = {0};
 
-
 	process_inputs(items, weights, values); // processing the user input
-
-	printf("items\n");
-	for (int i = 0; i < 5; ++i)
-	{
-		printf("%c",items[i]);
-	}
-	printf("\nvalues");
-	printf("\n");
-	for (int i = 0; i < 5; ++i)
-	{
-		printf("%i ",values[i]);
-	}
-	printf("\nWeights");
-	printf("\n");
-
-	for (int i = 0; i <5; ++i)
-	{
-		printf("%i ",weights[i]);
-	}
 
 	int maximum_val = knapSack(weights, values, selected_bool); // calculating the optimal weight
 
 	//prints
-	printf("\nMaximum profit: %d", maximum_val); // the maximum value
+	printf("Maximum profit: %d", maximum_val); // the maximum value
 
-	char string[11];
-	create_string(string, selected_bool, items, weights);
-	printf("\nSelected items: %s", string);
-
-
+	char string[BACKPACKSIZE * 2 + 1];
+	create_string(string, selected_bool, items);
+	printf("\nSelected items: %s", string); // the picked items
 }
 
 
